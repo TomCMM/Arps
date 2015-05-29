42f5f7bb5b5593a115da7e45d2df480719f7b279
Thomas Christian <thomas@coxinha.(none)>
2015-05-29 18:58:48 -0300

    Try to delete

--- build/py2exe/source/Python-dynload.h
+++
@@ -1,9 +0,0 @@
-#define Py_OptimizeFlag *(_Py_OptimizeFlag_PTR())
-#define Py_NoSiteFlag *(_Py_NoSiteFlag_PTR())
-#define Py_VerboseFlag *(_Py_VerboseFlag_PTR())
-
-extern int *_Py_OptimizeFlag_PTR();
-extern int *_Py_NoSiteFlag_PTR();
-extern int *_Py_VerboseFlag_PTR();
-
-extern int PythonLoaded(HMODULE hmod);
